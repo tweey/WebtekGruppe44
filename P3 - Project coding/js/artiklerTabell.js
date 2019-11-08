426a14b27236f784cbc7446c044a09296ebd7814
Njål Østbø <noestboe@gmail.com>
2019-11-08 12:08:42 +0100

    La til artikler

---
+++ P3 - Project coding/js/artiklerTabell.js
@@ -0,0 +1,27 @@
+const articles = [
+	{
+		title: "Collaborative Security Risk Estimation in Agile Software Development",
+		link: "http://jaatun.no/papers/2019/ICS_protectionpoker_final_author.pdf'",
+		publishedDate: "2019-10-01",
+	},
+	{
+		title: "The Security Intention Meeting Series as a way to increase visibility of software security decisions in agile development projects",
+		link: "http://jaatun.no/papers/2019/security-intention.pdf",
+		publishedDate: "2019-09-01",
+	},
+	{
+		title: "Is a Smarter Grid Also Riskier?",
+		link: "http://jaatun.no/papers/2019/smartgridrisk.pdf",
+		publishedDate: "2019-10-01",
+	},
+	{
+		title: "Understanding challenges to adoption of the Protection Poker software security game",
+		link: "http://jaatun.no/papers/2019/understanding-protectionpoker.pdf",
+		publishedDate: "2019-10-01",
+	},
+	{
+		title: "A Secure MANET Routing Protocol for Crisis Situations",
+		link: "http://jaatun.no/papers/2018/A-Secure-MANET-Routing-Protocol-for-Crisis-Situations.pdf",
+		publishedDate: "2018-10-01",
+	}
+]
