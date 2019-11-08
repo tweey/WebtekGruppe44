b6bfbe0089f588bc11da32c16d120b47cd741e93
Endre Lem <endreale@stud.ntnu.no>
2019-11-08 12:55:24 +0100

    Småforbedringer

--- P3 - Project coding/test/artikler_test.js
+++ P3 - Project coding/test/artikler_test.js
@@ -7,48 +7,45 @@ const artikler = [
 	{
 		title: "The Security Intention Meeting Series as a way to increase visibility of software security decisions in agile development projects",
 		link: "http://jaatun.no/papers/2019/security-intention.pdf",
-		publishedDate: "2019-09-01",
+		publishedDate: "2019-09-29",
 	},
 	{
 		title: "Is a Smarter Grid Also Riskier?",
 		link: "http://jaatun.no/papers/2019/smartgridrisk.pdf",
-		publishedDate: "2019-10-01",
+		publishedDate: "2019-07-14",
 	},
 	{
 		title: "Understanding challenges to adoption of the Protection Poker software security game",
 		link: "http://jaatun.no/papers/2019/understanding-protectionpoker.pdf",
-		publishedDate: "2019-10-01",
+		publishedDate: "2019-03-03",
 	},
 	{
 		title: "A Secure MANET Routing Protocol for Crisis Situations",
 		link: "http://jaatun.no/papers/2018/A-Secure-MANET-Routing-Protocol-for-Crisis-Situations.pdf",
-		publishedDate: "2018-10-01",
+		publishedDate: "2018-11-19",
 	}
 ]
 
 var tableBody = document.getElementById('artikler-tabell-body');
 
-
-
 for (var i=0; i < artikler.length; i++) {
-	const article = artikler[i]	
+	const article = artikler[i];
 	var tr = document.createElement('tr');
 	var td = document.createElement('td');
 
+	const publishedElement = document.createElement('td');
+	publishedElement.appendChild(document.createTextNode(article.publishedDate));
+	tr.appendChild(publishedElement);
+	tableBody.appendChild(tr);
+
 	const titleElement = document.createElement('td');
-	titleElement.appendChild(document.createTextNode(article.title))
+	titleElement.appendChild(document.createTextNode(article.title));
 	tr.appendChild(titleElement);
 	tableBody.appendChild(tr);
 
 	const linkElement = document.createElement('td');
-	linkElement.appendChild(document.createTextNode(article.link))
+	linkElement.appendChild(document.createTextNode(article.link));
 	tr.appendChild(linkElement);
 	tableBody.appendChild(tr);
-
-	const publishedElement = document.createElement('td');
-	publishedElement.appendChild(document.createTextNode(article.publishedDate))
-	tr.appendChild(publishedElement);
-	tableBody.appendChild(tr);
-
 
 }
