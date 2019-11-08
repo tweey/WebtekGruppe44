22656c2d19a53167dd78ad83c5ab47c9026a8cfc
Endre Lem <endreale@stud.ntnu.no>
2019-11-08 13:05:32 +0100

    La til ny js-funksjon til artikler.html

--- P3 - Project coding/js/artikler.js
+++ P3 - Project coding/js/artikler.js
@@ -1,7 +1,7 @@
 // Dette scriptet skal omdirigere brukeren til en tilfeldig valgt nettside når vedkommende trykker på knappen
 
 // Starter med å lage en array for alle artikkel-lenkene
-var articles = ['http://jaatun.no/papers/2005/oban_nordsec.pdf',
+var articleLinks = ['http://jaatun.no/papers/2005/oban_nordsec.pdf',
         'http://jaatun.no/papers/2006/kerberos_ifipsec.pdf',
         'http://jaatun.no/papers/2008/testing.pdf',
         'http://jaatun.no/papers/2008/sesa_atc.pdf',
@@ -78,5 +78,65 @@ var articles = ['http://jaatun.no/papers/2005/oban_nordsec.pdf',
 function randomArticle() {
         var randomNumber = Math.round(Math.random() * 72);
 
-        window.location = articles[randomNumber];
+        window.location = articleLinks[randomNumber];
+}
+
+
+// Denne loopen skal appende gitte artikler til en tabell
+const articles = [
+        {
+                title: "Collaborative Security Risk Estimation in Agile Software Development",
+                link: "http://jaatun.no/papers/2019/ICS_protectionpoker_final_author.pdf'",
+                publishedDate: "2019-10-01",
+        },
+        {
+                title: "The Security Intention Meeting Series as a way to increase visibility of software security decisions in agile development projects",
+                link: "http://jaatun.no/papers/2019/security-intention.pdf",
+                publishedDate: "2019-09-29",
+        },
+        {
+                title: "Is a Smarter Grid Also Riskier?",
+                link: "http://jaatun.no/papers/2019/smartgridrisk.pdf",
+                publishedDate: "2019-07-14",
+        },
+        {
+                title: "Understanding challenges to adoption of the Protection Poker software security game",
+                link: "http://jaatun.no/papers/2019/understanding-protectionpoker.pdf",
+                publishedDate: "2019-03-03",
+        },
+        {
+                title: "A Secure MANET Routing Protocol for Crisis Situations",
+                link: "http://jaatun.no/papers/2018/A-Secure-MANET-Routing-Protocol-for-Crisis-Situations.pdf",
+                publishedDate: "2018-11-19",
+        }
+]
+
+// Henter tbody fra HTML
+var tableBody = document.getElementById('artikler-tabell-body');
+
+// For hver artikkel i articles[i] skal den appende gitt data
+for (var i=0; i < articles.length; i++) {
+        // Definerer array og lager tr og td til HTML
+        const article = articles[i];
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+
+        // Appender publishedData i array til HTML
+        const publishedElement = document.createElement('td');
+        publishedElement.appendChild(document.createTextNode(article.publishedDate));
+        tr.appendChild(publishedElement);
+        tableBody.appendChild(tr);
+
+        // Appender title i array til HTML
+        const titleElement = document.createElement('td');
+        titleElement.appendChild(document.createTextNode(article.title));
+        tr.appendChild(titleElement);
+        tableBody.appendChild(tr);
+
+        // Appender link i array til HTML
+        const linkElement = document.createElement('td');
+        linkElement.appendChild(document.createTextNode(article.link));
+        tr.appendChild(linkElement);
+        tableBody.appendChild(tr);
+
 }
