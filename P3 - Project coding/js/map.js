d17c143ea2ff261de1536ae2f1805cfef8259670
Tweey <nm149mc@gmail.com>
2019-11-09 22:36:52 +0100

    Ureponsivt kart

---
+++ P3 - Project coding/js/map.js
@@ -0,0 +1,7 @@
+function initMap() {
+    let kontor = {lat: 63.3934638, lng: 10.4065978}
+
+    let map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: kontor});
+
+    let marker = new google.maps.Marker({position: kontor, map: map});
+}
