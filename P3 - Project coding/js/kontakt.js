30d9b4361e7a9198992bd6c5213cd5a07ea088d8
Tweey <nm149mc@gmail.com>
2019-11-10 04:23:39 +0100

    capcha, mail oglittsånn... ikke designetgit statusgit statusgit status

--- P3 - Project coding/js/kontakt.js
+++ P3 - Project coding/js/kontakt.js
@@ -1,8 +1,43 @@
 // Viser en melding som bekreftelse når kunden har tastet inn informasjon:
+let kontaktinfo = document.querySelector(".kontakt");
+let btnEl = document.getElementById("btn");
 
-var kontaktinfo = document.querySelector(".kontakt");
+function verifisering() {
+    let divEl = document.createElement("div");
+    divEl.className = "popup";
+    let randomNr1  = Math.floor(Math.random() * 11);
+    let randomNr2 = Math.floor(Math.random() * 11);
+    let randomSum = randomNr1 * randomNr2;
+    let pEl = document.createElement("p");
+    let inputEl = document.createElement("input");
+    inputEl.type = "number";
+    let text = document.createTextNode("What is "+randomNr1+" * "+randomNr2+"?");
 
-function bekreftelse(){
-    alert("Din henvendelse er mottatt!")
+    pEl.appendChild(text)
+    divEl.appendChild(pEl)
+    divEl.appendChild(inputEl);
+
+    document.body.appendChild(divEl)
+
+    inputEl.addEventListener("input", () => {
+        if (parseInt(inputEl.value) === randomSum) {
+            document.body.removeChild(divEl)
+            sendMail()
+            kontaktinfo.submit();
+        }
+    })
+    
 }
-kontaktinfo.addEventListener('submit',bekreftelse,true);
+
+function sendMail() {
+    let tittel = kontaktinfo.emne.value;
+    let innhold = kontaktinfo.melding.value;
+    window.open('mailto:test@example.com?subject='+tittel +'&body='+innhold);
+}
+        
+kontaktinfo.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    console.log(evt);
+    btnEl.disabled = true;
+    verifisering()
+})
